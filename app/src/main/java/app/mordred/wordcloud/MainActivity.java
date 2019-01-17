fbed370c125282aacd531cb3a7c18eabf300b8b3
sirmordred <oguzhan97yigit@gmail.com>
2019-01-17 21:09:43 +0300

    Add example app

--- app/src/main/java/app/mordred/wordcloud/MainActivity.java
+++ app/src/main/java/app/mordred/wordcloud/MainActivity.java
@@ -1,7 +1,15 @@
 package app.mordred.wordcloud;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.mordred.wordcloud.WordCloud;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imgView = findViewById(R.id.imageView);
+
+        Map<String, Integer> nMap = new HashMap<>();
+
+        nMap.put("oguzhan", 2);
+        nMap.put("mordred", 2);
+        nMap.put("is", 4);
+        nMap.put("on",2);
+        nMap.put("the", 3);
+        nMap.put("salda lake",5);
+
+        WordCloud wd = new WordCloud(nMap, 250, 250,Color.BLACK,Color.WHITE);
+        wd.setWordColorOpacityAuto(true);
+
+        Bitmap generatedWordCloudBmp = wd.generate();
+
+        imgView.setImageBitmap(generatedWordCloudBmp);
+
     }
 }
