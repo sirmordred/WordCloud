56207f1747654f4133e357ea3b5ddf3f272526de
sirmordred <oguzhan97yigit@gmail.com>
2019-01-17 15:11:42 +0300

    Fixups and improvements * Use getY() instead of wordRect.bottom for calc. output Bitmap Height * Fixup some TODOs * Add some more TODOs

--- Wordcloud/src/main/java/com/mordred/wordcloud/WordCloud.java
+++ Wordcloud/src/main/java/com/mordred/wordcloud/WordCloud.java
@@ -37,7 +37,8 @@ public class WordCloud {
         this.dimenHeight = dimenHeight;
     }
 
-    public WordCloud(Map<String, Integer> wordMap, int dimenWidth, int dimenHeight, int defaultWordColor, int defaultBackgroundColor) {
+    public WordCloud(Map<String, Integer> wordMap, int dimenWidth, int dimenHeight,
+                     int defaultWordColor, int defaultBackgroundColor) {
         this.wordMap.putAll(wordMap);
         this.dimenWidth = dimenWidth;
         this.dimenHeight = dimenHeight;
@@ -73,13 +74,13 @@ public class WordCloud {
                     }
                 }
             }
-            int cHeight = 0;
+            float cHeight = 0;
             for (Word w3: wordList) {
-                if (w3.getWordRect().bottom > cHeight) {
-                    cHeight = w3.getWordRect().bottom;
+                if (w3.getY() > cHeight) {
+                    cHeight = w3.getY();
                 }
             }
-            calculatedHeight = cHeight;
+            calculatedHeight = (int) cHeight;
             if (calculatedHeight > dimenHeight) {
                 // it means its too large to fit so decrease font size
                 return 0;
@@ -109,11 +110,12 @@ public class WordCloud {
 
         List<Word> finalWordList = new ArrayList<>();
 
-        List<Map.Entry<String, Integer>> wordMapEntries = new ArrayList<>(wordMap.entrySet());
-        sortAsDescending(wordMapEntries);
-        int largestWordCnt = wordMapEntries.get(0).getValue();
+        List<Integer> countList = new ArrayList<>(wordMap.values());
+        Collections.sort(countList, Collections.reverseOrder());
+        int largestWordCnt = countList.get(0);
 
-        for (int newMaxFontSize = this.maxFontSize; newMaxFontSize > this.minFontSize; newMaxFontSize--) {
+        for (int newMaxFontSize = this.maxFontSize; newMaxFontSize > this.minFontSize;
+             newMaxFontSize--) {
             finalWordList.clear();
             for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
                 float calculatedTextSize = getTextSize(entry.getValue(),largestWordCnt,
@@ -125,11 +127,13 @@ public class WordCloud {
             int fitResult = fit(finalWordList);
             if (fitResult == 1) {
                 break;
+            } else if (fitResult == -1) {
+                return null;
             }
         }
 
         // draw it
-        Bitmap retBitmap = Bitmap.createBitmap(dimenWidth + 10,dimenHeight + 10,Bitmap.Config.ARGB_8888); // TODO +10 for stability, remove them in future
+        Bitmap retBitmap = Bitmap.createBitmap(dimenWidth,dimenHeight,Bitmap.Config.ARGB_8888);
         Canvas retBitmapCanvas = new Canvas(retBitmap);
         retBitmapCanvas.drawColor(defaultBackgroundColor); // background
 
@@ -143,22 +147,19 @@ public class WordCloud {
         return retBitmap;
     }
 
-    private void sortAsDescending(List<Map.Entry<String, Integer>> unsortedList) {
-        Collections.sort(unsortedList, Collections.reverseOrder(new Comparator<Map.Entry<String,
-                Integer>>() {
-            public int compare(Map.Entry<String, Integer> left,
-                               Map.Entry<String, Integer> right) {
-                return Integer.compare(left.getValue(), right.getValue());
-            }
-        }));
-    }
-
-    private float getTextSize(int wordCount, int largestWordCount, int maxFontSize, int minFontSize) {
+    private float getTextSize(int wordCount, int largestWordCount,
+                              int maxFontSize, int minFontSize) {
         return (float) (largestWordCount > 1 ? Math.log(wordCount) / Math.log(largestWordCount) *
                 (maxFontSize - minFontSize) + minFontSize : minFontSize);
     }
 
-    public void setDefaultWordColor(int defaultWordColor) { // TODO otherwise every word will get random rgb
+    /* TODO like on getTextSize(), calculate word colors opacity by their frequency(count)
+    private int getTextColor(int wordCount, int largestWordCount,
+                              int maxFontSize, int minFontSize) {
+        return;
+    } */
+
+    public void setDefaultWordColor(int defaultWordColor) {
         this.defaultWordColor = defaultWordColor;
     }
 
